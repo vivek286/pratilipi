a33fe7840ae7be3f4523a1531b0abcc582a9e453
vivek286 <solankivivek01@gmail.com>
2022-03-12 01:46:00 +0530

    ss

--- pages/display.js
+++ pages/display.js
@@ -11,9 +11,9 @@ const  Home = (props) => {
         {/* <p>book:- {props.data.name?props.data.name:""} <br/> by:- {props.data.by?props.data.by:""}</p> */}
 </div>
 <div className={Style.content}>
-    Book Description <br/>{props.data.content?props.data.content:""}
+    {/* Book Description <br/>{props.data.content?props.data.content:""} */}
 </div>
-<a href={`/view/${String(props.iddd)}-${String(props.ind+1)}`}><button >View Chapters</button></a>
+{/* <a href={`/view/${String(props.iddd)}-${String(props.ind+1)}`}><button >View Chapters</button></a> */}
 
       
     </div>

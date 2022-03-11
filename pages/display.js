871a459fbe10f7abaf12daebd07693c13dff6377
vivek286 <solankivivek01@gmail.com>
2022-03-12 01:20:31 +0530

    add too

--- pages/display.js
+++ pages/display.js
@@ -8,10 +8,10 @@ const  Home = (props) => {
   return (
   <div className={Style.body}>
     <div className={Style.header}>
-        <p>book:- {props.data.name} <br/> by:- {props.data.by}</p>
+        <p>book:- {props.data.name?props.data.name:""} <br/> by:- {props.data.by?props.data.by:""}</p>
 </div>
 <div className={Style.content}>
-    Book Description <br/>{props.data.content}
+    Book Description <br/>{props.data.content?props.data.content:""}
 </div>
 <a href={`/view/${String(props.iddd)}-${String(props.ind+1)}`}><button >View Chapters</button></a>
 

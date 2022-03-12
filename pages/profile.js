a1570d2d6b00484e64b8483bd5323f1ed8447daf
vivek286 <solankivivek01@gmail.com>
2022-03-12 17:23:26 +0530

    added

--- pages/profile.js
+++ pages/profile.js
@@ -1,22 +1,14 @@
 import { useUser } from '../lib/hooks'
 import Layout from '../components/layout'
-import data from './docs/data.json'
+
 const Profile = () => {
   const user = useUser({ redirectTo: '/login' })
-  async function adddatatodb(){
-  //   const response=await fetch('./api/add_story',{
-  //     method:'POST',
-  // body:(JSON.stringify(data)),
-  // headers:{
-  //     'content-type':'application/json'
-  // }
-  //   })
-    // console.log(response);
-  }
+ 
+  
   return (
     <Layout>
       <h1>Profile</h1>
-      {/* <button onClick={adddatatodb()}>Send to db</button> */}
+     
       {user && (
         <>
           <p>Your session:</p>
